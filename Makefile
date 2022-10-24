lwfinger/rtw89

commit bc9e12c479d3efdc9b33aa460715afe8cc9b129f

rtw89: Add firmware for 8852be and 8852ce
Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

Makefile
@@ -56,6 +56,7 @@ rtw_8852ae-y := rtw8852be.o
 obj-m += rtw_8852b.o
 rtw_8852b-y := rtw8852b.o \
 		    rtw8852b_table.o \
+		    rtw8852b_rfk.o \
 		    rtw8852b_rfk_table.o
 
 obj-m += rtw_8852be.o
@@ -103,6 +104,7 @@ endif
 	@echo "Install rtw89 SUCCESS"
 
 uninstall:
+	@cp *.bin /lib/firmware/rtw89/.
 	@rm -f $(MODDESTDIR)/rtw89*.ko
 
 	@depmod -a

commit 110f7d7b77976b122e5573e0c14ae57e57dda1d7

fix: do not copy firmware during uninstall, remove it instead
Fixes: bc9e12c ("rtw89: Add firmware for 8852be and 8852ce")

Makefile
@@ -97,14 +97,19 @@ endif
 	@depmod -a $(KVER)
 
 	@mkdir -p /lib/firmware/rtw89/
-	@cp rtw8852a_fw.bin /lib/firmware/rtw89/.
+	@cp \
+		rtw8852a_fw.bin \
+		rtw8852b_fw.bin \
+		rtw8852c_fw.bin \
+		/lib/firmware/rtw89/.
 	@mkdir -p /lib/firmware/rtl_bt/
 	@cp rtl8852au*.bin /lib/firmware/rtl_bt/.
 
 	@echo "Install rtw89 SUCCESS"
 
 uninstall:
-	@cp *.bin /lib/firmware/rtw89/.
+	@rm -f /lib/firmware/rtw89/*bin
+	@rm -f /lib/firmware/rtl_bt/*bin
 	@rm -f $(MODDESTDIR)/rtw89*.ko
 
 	@depmod -a
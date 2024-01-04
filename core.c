lwfinger/rtw89

commit f247fefa2138a0efe0de0f95330e3670e7165a43

Added support for RHEL9

core.c
@@ -487,17 +487,17 @@ rtw89_core_tx_update_ampdu_info(struct rtw89_dev *rtwdev,
 
 	ampdu_num = (u8)((rtwsta->ampdu_params[tid].agg_num ?
 			  rtwsta->ampdu_params[tid].agg_num :
-#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 19, 0)
-			  4 << sta->ht_cap.ampdu_factor) - 1);
-#else
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 19, 0) || (RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(9, 0)))
 			  4 << sta->deflink.ht_cap.ampdu_factor) - 1);
+#else
+			  4 << sta->ht_cap.ampdu_factor - 1;
 #endif
 
 	desc_info->agg_en = true;
-#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 19, 0)
-	desc_info->ampdu_density = sta->ht_cap.ampdu_density;
-#else
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 19, 0) || (RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(9, 0)))
 	desc_info->ampdu_density = sta->deflink.ht_cap.ampdu_density;
+#else	
+	desc_info->ampdu_density = sta->ht_cap.ampdu_density;
 #endif
 	desc_info->ampdu_num = ampdu_num;
 }
@@ -710,10 +710,10 @@ __rtw89_core_tx_check_he_qos_htc(struct rtw89_dev *rtwdev,
 	if (pkt_type < PACKET_MAX)
 		return false;
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 19, 0)
-	if (!sta || !sta->he_cap.has_he)
-#else
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 19, 0) || (RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(9, 0)))
 	if (!sta || !sta->deflink.he_cap.has_he)
+#else		
+	if (!sta || !sta->he_cap.has_he)
 #endif
 		return false;
 
@@ -804,17 +804,17 @@ static u16 rtw89_core_get_data_rate(struct rtw89_dev *rtwdev,
 	else
 		lowest_rate = RTW89_HW_RATE_OFDM6;
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 19, 0)
-	if (!sta || !sta->supp_rates[chan->band_type])
-#else
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 19, 0) || (RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(9, 0)))
 	if (!sta || !sta->deflink.supp_rates[chan->band_type])
+#else		
+	if (!sta || !sta->supp_rates[chan->band_type])
 #endif
 		return lowest_rate;
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 19, 0)
-	return __ffs(sta->supp_rates[chan->band_type]) + lowest_rate;
-#else
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 19, 0) || (RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(9, 0)))
 	return __ffs(sta->deflink.supp_rates[chan->band_type]) + lowest_rate;
+#else	
+	return __ffs(sta->supp_rates[chan->band_type]) + lowest_rate;
 #endif
 }
 
@@ -2568,7 +2568,7 @@ EXPORT_SYMBOL(rtw89_core_napi_stop);
 void rtw89_core_napi_init(struct rtw89_dev *rtwdev)
 {
 	init_dummy_netdev(&rtwdev->netdev);
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 1, 0)
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 1, 0) || (RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(9, 0)))
 	netif_napi_add(&rtwdev->netdev, &rtwdev->napi,
 		       rtwdev->hci.ops->napi_poll);
 #else
@@ -2936,7 +2936,7 @@ static int rtw89_core_send_nullfunc(struct rtw89_dev *rtwdev,
 	struct sk_buff *skb;
 	int ret, qsel;
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 0, 0)
+#if ((LINUX_VERSION_CODE >= KERNEL_VERSION(6, 0, 0)) || (RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(9, 0)))
 	if (vif->type != NL80211_IFTYPE_STATION || !vif->cfg.assoc)
 #else
 	if (vif->type != NL80211_IFTYPE_STATION || !vif->bss_conf.assoc)
@@ -2950,7 +2950,7 @@ static int rtw89_core_send_nullfunc(struct rtw89_dev *rtwdev,
 		goto out;
 	}
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 1, 0)
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 1, 0)  || (RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(9, 0)))
 	skb = ieee80211_nullfunc_get(rtwdev->hw, vif, -1, qos);
 #else
 	skb = ieee80211_nullfunc_get(rtwdev->hw, vif, qos);
@@ -3908,7 +3908,7 @@ static void rtw89_init_he_cap(struct rtw89_dev *rtwdev,
 				  IEEE80211_HE_PHY_CAP9_RX_1024_QAM_LESS_THAN_242_TONE_RU |
 				  IEEE80211_HE_PHY_CAP9_RX_FULL_BW_SU_USING_MU_WITH_COMP_SIGB |
 				  IEEE80211_HE_PHY_CAP9_RX_FULL_BW_SU_USING_MU_WITH_NON_COMP_SIGB |
-#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 17, 0)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 17, 0) && (RHEL_RELEASE_CODE < RHEL_RELEASE_VERSION(9, 0))
 				  IEEE80211_HE_PHY_CAP9_NOMIMAL_PKT_PADDING_16US;
 #else
 				  u8_encode_bits(IEEE80211_HE_PHY_CAP9_NOMINAL_PKT_PADDING_16US,

cam.c
@@ -734,7 +734,7 @@ void rtw89_cam_fill_addr_cam_info(struct rtw89_dev *rtwdev,
 	FWCMD_SET_ADDR_FRM_TGT_IND(cmd, rtwvif->frm_tgt_ind);
 	FWCMD_SET_ADDR_MACID(cmd, rtwsta ? rtwsta->mac_id : rtwvif->mac_id);
 	if (rtwvif->net_type == RTW89_NET_TYPE_INFRA)
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 0, 0))
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 0, 0) || (RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(9, 0)))
 		FWCMD_SET_ADDR_AID12(cmd, vif->cfg.aid & 0xfff);
 #else
 		FWCMD_SET_ADDR_AID12(cmd, vif->bss_conf.aid & 0xfff);

coex.c
@@ -5902,7 +5902,17 @@ void rtw89_btc_ntfy_role_info(struct rtw89_dev *rtwdev, struct rtw89_vif *rtwvif
 
 		rtw89_debug(rtwdev, RTW89_DBG_BTC,
 			    "[BTC], STA support HE=%d VHT=%d HT=%d\n",
-#if LINUX_VERSION_CODE < KERNEL_VERSION(5,19,0)
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5,19,0) || (RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(9, 0)))
+			    sta->deflink.he_cap.has_he,
+                            sta->deflink.vht_cap.vht_supported,
+                            sta->deflink.ht_cap.ht_supported);
+                if (sta->deflink.he_cap.has_he)
+                        mode |= BIT(BTC_WL_MODE_HE);
+                if (sta->deflink.vht_cap.vht_supported)
+                        mode |= BIT(BTC_WL_MODE_VHT);
+                if (sta->deflink.ht_cap.ht_supported)
+                        mode |= BIT(BTC_WL_MODE_HT);
+#else
 			    sta->he_cap.has_he,
 			    sta->vht_cap.vht_supported,
 			    sta->ht_cap.ht_supported);
@@ -5911,16 +5921,6 @@ void rtw89_btc_ntfy_role_info(struct rtw89_dev *rtwdev, struct rtw89_vif *rtwvif
 		if (sta->vht_cap.vht_supported)
 			mode |= BIT(BTC_WL_MODE_VHT);
 		if (sta->ht_cap.ht_supported)
-			mode |= BIT(BTC_WL_MODE_HT);
-#else
-			    sta->deflink.he_cap.has_he,
-			    sta->deflink.vht_cap.vht_supported,
-			    sta->deflink.ht_cap.ht_supported);
-		if (sta->deflink.he_cap.has_he)
-			mode |= BIT(BTC_WL_MODE_HE);
-		if (sta->deflink.vht_cap.vht_supported)
-			mode |= BIT(BTC_WL_MODE_VHT);
-		if (sta->deflink.ht_cap.ht_supported)
 			mode |= BIT(BTC_WL_MODE_HT);
 #endif
 

core.h
@@ -5571,7 +5571,7 @@ void rtw89_chip_cfg_txpwr_ul_tb_offset(struct rtw89_dev *rtwdev,
 	struct rtw89_vif *rtwvif = (struct rtw89_vif *)vif->drv_priv;
 	const struct rtw89_chip_info *chip = rtwdev->chip;
 
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 0, 0))
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 0, 0)) || (RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(9, 0))
 	if (!vif->bss_conf.he_support || !vif->cfg.assoc)
 #else
 	if (!vif->bss_conf.he_support || !vif->bss_conf.assoc)
@@ -5693,19 +5693,19 @@ static inline u8 *get_hdr_bssid(struct ieee80211_hdr *hdr)
 
 static inline bool rtw89_sta_has_beamformer_cap(struct ieee80211_sta *sta)
 {
-#if LINUX_VERSION_CODE < KERNEL_VERSION(5,19,0)
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5,19,0) && (RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(9, 0)))
+	if ((sta->deflink.vht_cap.cap & IEEE80211_VHT_CAP_MU_BEAMFORMER_CAPABLE) ||
+            (sta->deflink.vht_cap.cap & IEEE80211_VHT_CAP_SU_BEAMFORMER_CAPABLE) ||
+            (sta->deflink.he_cap.he_cap_elem.phy_cap_info[3] &
+                        IEEE80211_HE_PHY_CAP3_SU_BEAMFORMER) ||
+            (sta->deflink.he_cap.he_cap_elem.phy_cap_info[4] &
+                        IEEE80211_HE_PHY_CAP4_MU_BEAMFORMER))
+                return true;
+#else
 	if ((sta->vht_cap.cap & IEEE80211_VHT_CAP_MU_BEAMFORMER_CAPABLE) ||
 	    (sta->vht_cap.cap & IEEE80211_VHT_CAP_SU_BEAMFORMER_CAPABLE) ||
 	    (sta->he_cap.he_cap_elem.phy_cap_info[3] & IEEE80211_HE_PHY_CAP3_SU_BEAMFORMER) ||
 	    (sta->he_cap.he_cap_elem.phy_cap_info[4] & IEEE80211_HE_PHY_CAP4_MU_BEAMFORMER))
-		return true;
-#else
-	if ((sta->deflink.vht_cap.cap & IEEE80211_VHT_CAP_MU_BEAMFORMER_CAPABLE) ||
-	    (sta->deflink.vht_cap.cap & IEEE80211_VHT_CAP_SU_BEAMFORMER_CAPABLE) ||
-	    (sta->deflink.he_cap.he_cap_elem.phy_cap_info[3] &
-			IEEE80211_HE_PHY_CAP3_SU_BEAMFORMER) ||
-	    (sta->deflink.he_cap.he_cap_elem.phy_cap_info[4] &
-			IEEE80211_HE_PHY_CAP4_MU_BEAMFORMER))
 		return true;
 #endif
 	return false;
@@ -5902,6 +5902,12 @@ void rtw89_core_stop(struct rtw89_dev *rtwdev);
 	} \
 	(cond) ? 0 : -ETIMEDOUT; \
 })
+
+#ifndef RHEL_RELEASE_CODE
+#define RHEL_RELEASE_VERSION(a,b) (((a) << 8) + (b))
+#define RHEL_RELEASE_CODE 0
+#endif
+
 
 #if defined(CONFIG_SUSE_VERSION)
 #if CONFIG_SUSE_PATCHLEVEL && CONFIG_SUSE_VERSION == 15 && CONFIG_SUSE_PATCHLEVEL == 3

debug.c
@@ -3518,7 +3518,7 @@ static void rtw89_sta_info_get_iter(void *data, struct ieee80211_sta *sta)
 	seq_printf(m, " BW:%u", rtw89_rate_info_bw_to_mhz(rate->bw));
 	seq_printf(m, "\t(hw_rate=0x%x)", rtwsta->ra_report.hw_rate);
 	seq_printf(m, "\t==> agg_wait=%d (%d)\n", rtwsta->max_agg_wait,
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 1, 0)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 1, 0) || (RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(9, 0))
 		   sta->deflink.agg.max_rc_amsdu_len);
 #else
 		   sta->max_rc_amsdu_len);

fw.c
@@ -1670,17 +1670,17 @@ static int rtw89_fw_h2c_add_general_pkt(struct rtw89_dev *rtwdev,
 		skb = ieee80211_proberesp_get(rtwdev->hw, vif);
 		break;
 	case RTW89_PKT_OFLD_TYPE_NULL_DATA:
-#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 1, 0)
-		skb = ieee80211_nullfunc_get(rtwdev->hw, vif, false);
-#else
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 1, 0) || (RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(9, 0)))
 		skb = ieee80211_nullfunc_get(rtwdev->hw, vif, -1, false);
+#else
+		skb = ieee80211_nullfunc_get(rtwdev->hw, vif, false);
 #endif
 		break;
 	case RTW89_PKT_OFLD_TYPE_QOS_NULL:
-#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 1, 0)
-		skb = ieee80211_nullfunc_get(rtwdev->hw, vif, true);
-#else
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 1, 0) || (RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(9, 0)))
 		skb = ieee80211_nullfunc_get(rtwdev->hw, vif, -1, true);
+#else
+		skb = ieee80211_nullfunc_get(rtwdev->hw, vif, true);
 #endif
 		break;
 	default:
@@ -1951,43 +1951,43 @@ static void __get_sta_he_pkt_padding(struct rtw89_dev *rtwdev,
 {
 	bool ppe_th;
 	u8 ppe16, ppe8;
-#if LINUX_VERSION_CODE < KERNEL_VERSION(5,19,0)
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5,19,0) || (RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(9, 0)))
+	u8 nss = min(sta->deflink.rx_nss, rtwdev->hal.tx_nss) - 1;
+        u8 ppe_thres_hdr = sta->deflink.he_cap.ppe_thres[0];
+#else
 	u8 nss = min(sta->rx_nss, rtwdev->hal.tx_nss) - 1;
 	u8 ppe_thres_hdr = sta->he_cap.ppe_thres[0];
-#else
-	u8 nss = min(sta->deflink.rx_nss, rtwdev->hal.tx_nss) - 1;
-	u8 ppe_thres_hdr = sta->deflink.he_cap.ppe_thres[0];
 #endif
 	u8 ru_bitmap;
 	u8 n, idx, sh;
 	u16 ppe;
 	int i;
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(5,19,0)
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5,19,0) || (RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(9, 0)))
+	if (!sta->deflink.he_cap.has_he)
+                return;
+
+        ppe_th = FIELD_GET(IEEE80211_HE_PHY_CAP6_PPE_THRESHOLD_PRESENT,
+                           sta->deflink.he_cap.he_cap_elem.phy_cap_info[6]);
+#else
 	if (!sta->he_cap.has_he)
 		return;
 
 	ppe_th = FIELD_GET(IEEE80211_HE_PHY_CAP6_PPE_THRESHOLD_PRESENT,
 			   sta->he_cap.he_cap_elem.phy_cap_info[6]);
-#else
-	if (!sta->deflink.he_cap.has_he)
-		return;
-
-	ppe_th = FIELD_GET(IEEE80211_HE_PHY_CAP6_PPE_THRESHOLD_PRESENT,
-			   sta->deflink.he_cap.he_cap_elem.phy_cap_info[6]);
 #endif
 	if (!ppe_th) {
 		u8 pad;
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 17, 0)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 17, 0)
 		pad = FIELD_GET(IEEE80211_HE_PHY_CAP9_NOMIMAL_PKT_PADDING_MASK,
 #else
 		pad = FIELD_GET(IEEE80211_HE_PHY_CAP9_NOMINAL_PKT_PADDING_MASK,
 #endif
-#if LINUX_VERSION_CODE < KERNEL_VERSION(5,19,0)
-				sta->he_cap.he_cap_elem.phy_cap_info[9]);
-#else
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5,19,0) || (RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(9, 0)))
 				sta->deflink.he_cap.he_cap_elem.phy_cap_info[9]);
+#else
+				sta->he_cap.he_cap_elem.phy_cap_info[9]);
 #endif
 
 		for (i = 0; i < RTW89_PPE_BW_NUM; i++)
@@ -2010,10 +2010,10 @@ static void __get_sta_he_pkt_padding(struct rtw89_dev *rtwdev,
 		sh = n & 7;
 		n += IEEE80211_PPE_THRES_INFO_PPET_SIZE * 2;
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(5,19,0)
-		ppe = le16_to_cpu(*((__le16 *)&sta->he_cap.ppe_thres[idx]));
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5,19,0) || (RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(9, 0)))
+                ppe = le16_to_cpu(*((__le16 *)&sta->deflink.he_cap.ppe_thres[idx]));
 #else
-		ppe = le16_to_cpu(*((__le16 *)&sta->deflink.he_cap.ppe_thres[idx]));
+		ppe = le16_to_cpu(*((__le16 *)&sta->he_cap.ppe_thres[idx]));
 #endif
 		ppe16 = (ppe >> sh) & IEEE80211_PPE_THRES_NSS_MASK;
 		sh += IEEE80211_PPE_THRES_INFO_PPET_SIZE;
@@ -2084,7 +2084,7 @@ int rtw89_fw_h2c_assoc_cmac_tbl(struct rtw89_dev *rtwdev,
 		SET_CMC_TBL_NOMINAL_PKT_PADDING160(skb->data, pads[RTW89_CHANNEL_WIDTH_160]);
 	}
 	if (sta)
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,19,0)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,19,0) || (RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(9, 0))
 		SET_CMC_TBL_BSR_QUEUE_SIZE_FORMAT(skb->data,
 						  sta->deflink.he_cap.has_he);
 #else
@@ -2215,7 +2215,7 @@ int rtw89_fw_h2c_update_beacon(struct rtw89_dev *rtwdev,
 	else
 		beacon_rate = RTW89_HW_RATE_OFDM6;
 
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 0, 0))
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 0, 0) || (RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(9, 0)))
 	skb_beacon = ieee80211_beacon_get_tim(rtwdev->hw, vif, &tim_offset,
 					      NULL, 0);
 #else

mac.c
@@ -5619,10 +5619,10 @@ static int rtw89_mac_set_csi_para_reg_ax(struct rtw89_dev *rtwdev,
 	u8 nc = 1, nr = 3, ng = 0, cb = 1, cs = 1, ldpc_en = 1, stbc_en = 1;
 	u8 port_sel = rtwvif->port;
 	u8 sound_dim = 3, t;
-#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 19, 0)
-	u8 *phy_cap = sta->he_cap.he_cap_elem.phy_cap_info;
-#else
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 19, 0) || (RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(9, 0)))
 	u8 *phy_cap = sta->deflink.he_cap.he_cap_elem.phy_cap_info;
+#else
+	u8 *phy_cap = sta->he_cap.he_cap_elem.phy_cap_info;
 #endif
 	u32 reg;
 	u16 val;
@@ -5640,20 +5640,20 @@ static int rtw89_mac_set_csi_para_reg_ax(struct rtw89_dev *rtwdev,
 			      phy_cap[5]);
 		sound_dim = min(sound_dim, t);
 	}
-#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 19, 0)
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 19, 0) || (RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(9, 0)))
+	if ((sta->deflink.vht_cap.cap & IEEE80211_VHT_CAP_MU_BEAMFORMER_CAPABLE) ||
+            (sta->deflink.vht_cap.cap & IEEE80211_VHT_CAP_SU_BEAMFORMER_CAPABLE)) {
+                ldpc_en &= !!(sta->deflink.vht_cap.cap & IEEE80211_VHT_CAP_RXLDPC);
+                stbc_en &= !!(sta->deflink.vht_cap.cap & IEEE80211_VHT_CAP_RXSTBC_MASK);
+                t = FIELD_GET(IEEE80211_VHT_CAP_SOUNDING_DIMENSIONS_MASK,
+                              sta->deflink.vht_cap.cap);
+#else
 	if ((sta->vht_cap.cap & IEEE80211_VHT_CAP_MU_BEAMFORMER_CAPABLE) ||
 	    (sta->vht_cap.cap & IEEE80211_VHT_CAP_SU_BEAMFORMER_CAPABLE)) {
 		ldpc_en &= !!(sta->vht_cap.cap & IEEE80211_VHT_CAP_RXLDPC);
 		stbc_en &= !!(sta->vht_cap.cap & IEEE80211_VHT_CAP_RXSTBC_MASK);
 		t = FIELD_GET(IEEE80211_VHT_CAP_SOUNDING_DIMENSIONS_MASK,
 			      sta->vht_cap.cap);
-#else
-	if ((sta->deflink.vht_cap.cap & IEEE80211_VHT_CAP_MU_BEAMFORMER_CAPABLE) ||
-	    (sta->deflink.vht_cap.cap & IEEE80211_VHT_CAP_SU_BEAMFORMER_CAPABLE)) {
-		ldpc_en &= !!(sta->deflink.vht_cap.cap & IEEE80211_VHT_CAP_RXLDPC);
-		stbc_en &= !!(sta->deflink.vht_cap.cap & IEEE80211_VHT_CAP_RXSTBC_MASK);
-		t = FIELD_GET(IEEE80211_VHT_CAP_SOUNDING_DIMENSIONS_MASK,
-			      sta->deflink.vht_cap.cap);
 #endif
 		sound_dim = min(sound_dim, t);
 	}
@@ -5695,28 +5695,28 @@ static int rtw89_mac_csi_rrsc_ax(struct rtw89_dev *rtwdev,
 	if (ret)
 		return ret;
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 19, 0)
-	if (sta->he_cap.has_he) {
-#else
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 19, 0) || (RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(9, 0)))
 	if (sta->deflink.he_cap.has_he) {
+#else
+	if (sta->he_cap.has_he) {
 #endif
 		rrsc |= (BIT(RTW89_MAC_BF_RRSC_HE_MSC0) |
 			 BIT(RTW89_MAC_BF_RRSC_HE_MSC3) |
 			 BIT(RTW89_MAC_BF_RRSC_HE_MSC5));
 	}
-#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 19, 0)
-	if (sta->vht_cap.vht_supported) {
-#else
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 19, 0) || (RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(9, 0)))
 	if (sta->deflink.vht_cap.vht_supported) {
+#else
+	if (sta->vht_cap.vht_supported) {
 #endif
 		rrsc |= (BIT(RTW89_MAC_BF_RRSC_VHT_MSC0) |
 			 BIT(RTW89_MAC_BF_RRSC_VHT_MSC3) |
 			 BIT(RTW89_MAC_BF_RRSC_VHT_MSC5));
 	}
-#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 19, 0)
-	if (sta->ht_cap.ht_supported) {
-#else
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 19, 0) || (RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(9, 0)))
 	if (sta->deflink.ht_cap.ht_supported) {
+#else
+	if (sta->ht_cap.ht_supported) {
 #endif
 		rrsc |= (BIT(RTW89_MAC_BF_RRSC_HT_MSC0) |
 			 BIT(RTW89_MAC_BF_RRSC_HT_MSC3) |

mac80211.c
@@ -432,7 +432,7 @@ static void rtw89_ops_bss_info_changed(struct ieee80211_hw *hw,
 	rtw89_leave_ps_mode(rtwdev);
 
 	if (changed & BSS_CHANGED_ASSOC) {
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 0, 0))
+#if ((LINUX_VERSION_CODE >= KERNEL_VERSION(6, 0, 0)) || (RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(9, 0)))
 		if (vif->cfg.assoc) {
 #else
 		if (conf->assoc) {
@@ -1147,11 +1147,13 @@ const struct ieee80211_ops rtw89_ops = {
 	.change_interface       = rtw89_ops_change_interface,
 	.remove_interface	= rtw89_ops_remove_interface,
 	.configure_filter	= rtw89_ops_configure_filter,
+#if RHEL_RELEASE_CODE > RHEL_RELEASE_VERSION(10, 0)	
 	.bss_info_changed	= rtw89_ops_bss_info_changed,
 	.start_ap		= rtw89_ops_start_ap,
 	.stop_ap		= rtw89_ops_stop_ap,
+	.conf_tx                = rtw89_ops_conf_tx,
+#endif	
 	.set_tim		= rtw89_ops_set_tim,
-	.conf_tx		= rtw89_ops_conf_tx,
 	.sta_state		= rtw89_ops_sta_state,
 	.set_key		= rtw89_ops_set_key,
 	.ampdu_action		= rtw89_ops_ampdu_action,
@@ -1169,8 +1171,10 @@ const struct ieee80211_ops rtw89_ops = {
 	.add_chanctx		= rtw89_ops_add_chanctx,
 	.remove_chanctx		= rtw89_ops_remove_chanctx,
 	.change_chanctx		= rtw89_ops_change_chanctx,
+#if RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(10, 0)    	
 	.assign_vif_chanctx	= rtw89_ops_assign_vif_chanctx,
 	.unassign_vif_chanctx	= rtw89_ops_unassign_vif_chanctx,
+#endif	
 	.remain_on_channel		= rtw89_ops_remain_on_channel,
 	.cancel_remain_on_channel	= rtw89_ops_cancel_remain_on_channel,
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 11, 0)

commit fcd883e116548bb049800bbcd5085e677406b913

rtw89: Fix build for kernel < 5.19.0
Fixes: f247fefa2138 ("Added support for RHEL9")

core.c
@@ -490,7 +490,7 @@ rtw89_core_tx_update_ampdu_info(struct rtw89_dev *rtwdev,
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 19, 0) || (RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(9, 0)))
 			  4 << sta->deflink.ht_cap.ampdu_factor) - 1);
 #else
-			  4 << sta->ht_cap.ampdu_factor - 1;
+			  4 << sta->ht_cap.ampdu_factor) - 1);
 #endif
 
 	desc_info->agg_en = true;